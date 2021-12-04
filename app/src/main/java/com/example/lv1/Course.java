a0460657dd1ac64bfe803b511ce5ffff798254d0
Majcenic <80134096+Majcenic@users.noreply.github.com>
2021-12-04 15:03:26 +0100

    Custom layout za predmet spinner

--- app/src/main/java/com/example/lv1/Course.java
+++ app/src/main/java/com/example/lv1/Course.java
@@ -4,11 +4,14 @@ import com.google.gson.annotations.SerializedName;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Course implements Serializable {
 
     public String title;
     public ArrayList<Instructor> instructors;
-    @SerializedName("expected_duration")
-    public int expectedDuration;
+    public String level;
+    public boolean starter;
+
+    @SerializedName("launch_date") public String launchDate;
 }
--- app/src/main/java/com/example/lv1/PredmetSpinnerAdapter.java
+++ app/src/main/java/com/example/lv1/PredmetSpinnerAdapter.java
@@ -1,14 +1,19 @@
 package com.example.lv1;
 
 import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.arch.core.util.Function;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class PredmetSpinnerAdapter extends ArrayAdapter<Course> {
@@ -40,18 +45,31 @@ public class PredmetSpinnerAdapter extends ArrayAdapter<Course> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
-        TextView label = (TextView) super.getView(position, convertView, parent);
-        label.setText(courses.get(position).title);
-
-        return label;
+        return setLayout(position, convertView, parent);
     }
 
     @Override
-    public View getDropDownView(int position, View convertView,
-                                ViewGroup parent) {
-        TextView label = (TextView) super.getDropDownView(position, convertView, parent);
-        label.setText(courses.get(position).title);
+    public View getDropDownView(int position, View convertView, ViewGroup parent) {
 
-        return label;
+        return setLayout(position, convertView, parent);
+    }
+
+    public View setLayout(int position, View convertView, ViewGroup parent)
+    {
+        if (convertView == null)
+        {
+            LayoutInflater vi = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = vi.inflate(R.layout.predmet_spinner_single_item, null);
+
+            Course selectedCourse = courses.get(position);
+
+            TextView textViewNaziv = (TextView) convertView.findViewById(R.id.spinner_txt_predmet_naziv);
+            textViewNaziv.setText(selectedCourse.title);
+
+            TextView textViewDetalji = (TextView) convertView.findViewById(R.id.spinner_txt_predmet_detalji);
+            textViewDetalji.setText(selectedCourse.level + (selectedCourse.starter ? " • starter" : ""));
+        }
+
+        return convertView;
     }
 }
